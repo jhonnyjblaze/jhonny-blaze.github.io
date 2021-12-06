88834c05d08ce67c2b3149c4ab8577f161cd39e0
Jhonny J. Blaze <jjamesii@gmail.com>
2021-12-06 20:55:13 +0000

    scratch-pad day-1

---
+++ projects/scratch-pad/day-1/stringy.js
@@ -0,0 +1,242 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: stringy.js
+ */
+
+/**
+ * Given an input String, return its length.
+ *
+ * TIP: In JavaScript, how can we decipher the length of a String?
+ * work?
+ */
+function length(string) {
+    // YOUR CODE BELOW HERE //
+    // Return the length of the string value
+    return string.length;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to lowercase.
+ */
+function toLowerCase(string) {
+    // YOUR CODE BELOW HERE //
+    // Return the string to its lowercased version
+    return string.toLowerCase();
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to uppercase.
+ */
+function toUpperCase(string) {
+    // YOUR CODE BELOW HERE //
+    // Return the string to its uppercased version
+    return string.toUpperCase();
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to dash-case.
+ *
+ * Examples:
+ *
+ *      toDashCase('Hello World'); // => 'hello-world'
+ *
+ * TIP: How can you look for and replace a space in a String? How can you
+ *      enforce lowercase? Look at the global replace link below, or, try String
+ *      methods split and join?
+ *
+ *      See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Using_global_and_ignore_with_replace()
+ */
+function toDashCase(string) {
+    // YOUR CODE BELOW HERE //
+    /* .split splits the string at each whitespace character then creates substrings in an array 
+       .join joins the substrings within the array with a dash forming a new string 
+       .toLowerCase() transforms the new string into its lowercased form.    
+    */
+    return string.split(' ').join('-').toLowerCase();
+    /* Replaces each and every whitespace in the string with a dash before making it lowercased 
+       /\s = match a single whitespace character
+        +/ = match the whitespace character one or more times
+         g = global search, return all charcter matches in the string
+    */
+    //return string.replace(/\s+/g, '-').toLowerCase();
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * begins with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      beginsWith('Max', 'm'); // => true;
+ *      beginsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function beginsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    // Checks if the lowercased version of the zero indexed character of the given string is equal to the lowercased version of the given char 
+    if (string[0].toLowerCase() == char.toLowerCase()) {
+        // If the check evaluates to true, returns true
+        return true;
+      // Otherwise returns false
+    } else return false;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * ends with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      endsWith('Max', 'X'); // => true;
+ *      endsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function endsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    // Check if the lowercased version of the last index character of the given string is equal to the lowercased version of the given char
+    if (string[string.length-1].toLowerCase() == char.toLowerCase()) {
+        // If the check evaluates to true, returns true
+        return true;
+      // Otherwise returns false  
+    } else return false;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two input Strings, return the Strings concatenated into one.
+ *
+ * TIP: What's the operator to concatenate two Strings?
+ */
+function concat(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    // Returns concatenated (added) stringOne and stringTwo with no whitespace between them
+    return stringOne + stringTwo;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given any number of Strings, return all of them joined together.
+ *
+ * Example:
+ *
+ *      join("my", "name", "is", "Ben"); // => "mynameisBen";
+ *
+ * TIP: This Function pulls out all the arguments passed to it and stores them
+ *      in an Array called args.
+ */
+function join(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    var args = Array.from(arguments);
+    // Returns args array elements joined without whitespace between the elements
+    return args.join('');
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return the longest of the two.
+ *
+ * Example:
+ *
+ *      longest("ben", "maggie");   //-> "maggie"
+ *
+ * TIP: What property of the String do we need to compare?
+ */
+function longest(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    // If the length of stringOne is greater than the length of stringtwo return stringOne
+    if(stringOne.length > stringTwo.length) {
+        return stringOne;
+      // Otherwise return stringTwo  
+    } else return stringTwo;
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return 1 if the first is higher in alphabetical order than
+ * the second, return -1 if the second is higher in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortAscending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    // Check if first argument is higher in alphabetical order than second
+    if (stringOne < stringTwo) {
+        // If check resolves to true return 1
+        return 1;
+      // Now check if first argument is lower in alphabetical order than second  
+    } else if (stringOne > stringTwo) {
+        // If check resolves to true return -1
+        return -1;
+      // Otherwise return 0  
+    } else return 0;
+        
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+/**
+ * Given two Strings, return 1 if the first is lower in alphabetical order than
+ * the second, return -1 if the second is lower in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortDescending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    // Check if first argument is lower in alphabetical order than second
+    if (stringOne > stringTwo) {
+        // If check resolves to true return 1
+        return 1;
+    // Now check if first argument is higher in alphabetical order than second  
+    } else if (stringOne < stringTwo) {
+        // If check resolves to true return -1
+        return -1;
+    // Otherwise return 0  
+    } else return 0;
+
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.length = length;
+    module.exports.toLowerCase = toLowerCase;
+    module.exports.toUpperCase = toUpperCase;
+    module.exports.toDashCase = toDashCase;
+    module.exports.beginsWith = beginsWith;
+    module.exports.endsWith = endsWith;
+    module.exports.concat = concat;
+    module.exports.join = join;
+    module.exports.longest = longest;
+    module.exports.sortAscending = sortAscending
+    module.exports.sortDescending = sortDescending;
+}
---
+++ setup.md
@@ -0,0 +1,32 @@
+// When you enter gitpod
+
+/*
+
+Step 1 each time you enter gitpod in the terminal type -- code ~/.bashrc
+Step 2 in the bashrc file type -- export CHE_PROJECTS_ROOT=/workspace
+Step 3 close bashrc file and close the terminal then open a new terminal and continue
+Step 4 in the terminal type -- sudo apt-get install subversion
+Step 5 in the terminal type -- npm install -g opspark
+
+*/
+
+/*
+OpSpark Tools
+
+Step 6 in the terminal type -- os install --> install OpSpark projects
+Step 7 add github username
+Step 8 add github personal access token
+
+os test - testing if project passes all tests prior to submission
+
+os submit - submit project to greenlight
+
+*/
+
+/*
+Git Commands
+
+git add -A
+git commit -m "description of changes"
+git push
+*/
