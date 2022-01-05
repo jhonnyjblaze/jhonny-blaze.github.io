2b7e35c33852cd67da83da9b46f1d65d41ec200c
Jhonny J. Blaze <jjamesii@gmail.com>
2022-01-05 04:00:45 +0000

    attempted divide

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -303,6 +303,19 @@ var multiply = function(x, y) {
 // 13. Write a function that divides two numbers without using the / operator  or
 // JavaScript's Math object.
 var divide = function(x, y) {
+  if (x === 0 && y === 0 || y === 0) {
+    return NaN;
+  }
+  if (x === 0) {
+    return 0;
+  }
+  
+  if (x > 0 || y < 0) { 
+    return 1 + divide(x - y, y);
+  }
+  if (x < 0 || y  > 0) {
+    return -1 + divide(x + y, y);
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers.  The GCD of two
